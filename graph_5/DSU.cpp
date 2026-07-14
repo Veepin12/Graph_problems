173549ffe4276821b3db8021b84f303c765bf234
Veepin_Kumar <veepin8459@gmail.com>
2026-07-14 22:41:09 +0530

    Disjoint set Union

---
+++ graph_5/DSU.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int get( vector<int> & parent , int x){
+
+    return parent[x] = parent[x] == x ? x : get( parent, parent[x]);
+
+}
+
+void Union(vector<int> & parent, vector<int> & rank , int a , int b){
+
+    a = get( parent , a);
+    b = get( parent , b);
+
+    if(rank[a] >= rank[b]){
+        rank[a]++;
+        parent[b] = a;
+    }
+    else{
+        rank[b]++;
+        parent[a] = b;
+    }
+
+
+    
+}
+
+int main(){
+
+    int n,m;
+
+    cin>>n>>m;
+
+    vector<int> parent( n+1);
+    vector<int> rank( n + 1, 0);
+
+    for( int i = 0; i <= n; i++){
+        parent[i] = i;
+    }
+
+    while(m--){
+
+        string str;
+        cin>>str;
+        if(str == "union"){
+            int x,y;
+            cin>>x>>y;
+            Union( parent , rank , x , y);
+
+
+        }
+        else{
+            int x;
+            cin>>x;
+            cout<<get(parent ,x)<<endl;
+        }
+
+    }
+    return 0;
+}
